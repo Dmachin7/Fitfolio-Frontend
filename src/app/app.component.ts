a6eaf3f86e523ca01e62881be89a5182ef8777d9
Daniel Machin <DanielTAYG@gmail.com>
2023-12-11 18:45:22 -0500

    sucesfully able to use backend info in the frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppServiceService } from './app-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Fitfolio-Frontend';
-}
+ 
+  constructor(private service : AppServiceService) {
+  
+  }
+
+workouts:any=[];
+  
+  
+  ngOnInit(){
+    this.getDataFromAPI();
+  }
+  
+  
+  getDataFromAPI() {
+  this.service.getData().subscribe((response) => {
+    this.workouts = response
+  }, (error) => {
+    console.log('Error is ', error)
+  })
+  }
+  
+  }
